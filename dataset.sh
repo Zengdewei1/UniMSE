63d2d9b206f94d7c674b4823bf3fbf1612f60ae9
zengdewei <2313970511@qq.com>
2023-11-06 14:37:11 +0800

    更新依赖

---
+++ dataset.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+
+# cd scratch place
+mkdir datasets
+cd datasets/
+
+# Download zip dataset from Google Drive
+# filename='OfficeHomeDataset_10072016.zip'
+# fileid='0B81rNlvomiwed0V1YUxQdC1uOTg'
+# wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=${fileid}' -O- | sed -rn 's/.confirm=([0-9A-Za-z_]+)./\1\n/p')&id=${fileid}" -O ${filename} && rm -rf /tmp/cookies.txt
+
+array=("10XPUZo8qb1ILF5_Z17AAODbrKX3_h4wL" "1pWH2xPVZFymxeJUrd6gF37qYbvmhh32s" "1NmlzPqOGiGaYbzfRi_12r2BjmKfGP3Hw")
+for item in "${array[@]}"; do
+    echo $item
+    wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=${fileid}' -O- | sed -rn 's/.confirm=([0-9A-Za-z_]+)./\1\n/p')&id=${fileid}" && rm -rf /tmp/cookies.txt    
+done
+
+# Unzip
+# unzip -q ${filename}
+# rm ${filename}
+# cd
