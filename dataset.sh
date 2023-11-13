f97b784d71c1568b1d23656e18654fdb6bc9016c
zengdewei <2313970511@qq.com>
2023-11-13 21:07:37 +0800

    更新下载

--- dataset.sh
+++ dataset.sh
@@ -9,13 +9,23 @@ cd datasets/
 # fileid='0B81rNlvomiwed0V1YUxQdC1uOTg'
 # wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=${fileid}' -O- | sed -rn 's/.confirm=([0-9A-Za-z_]+)./\1\n/p')&id=${fileid}" -O ${filename} && rm -rf /tmp/cookies.txt
 
-array=("10XPUZo8qb1ILF5_Z17AAODbrKX3_h4wL" "1pWH2xPVZFymxeJUrd6gF37qYbvmhh32s" "1NmlzPqOGiGaYbzfRi_12r2BjmKfGP3Hw")
-for item in "${array[@]}"; do
-    echo $item
+https://drive.google.com/file/d/1alEn_5RfHFXuu8jwEFMIbMOLF-28--k_/view?usp=sharing 
+text:https://drive.google.com/file/d/1F4K75-_aj29Qyd1xaA19p-LLA0hbI9g0/view?usp=sharing
+
+array=("10XPUZo8qb1ILF5_Z17AAODbrKX3_h4wL" "1pWH2xPVZFymxeJUrd6gF37qYbvmhh32s" "1NmlzPqOGiGaYbzfRi_12r2BjmKfGP3Hw" "1alEn_5RfHFXuu8jwEFMIbMOLF-28--k_" "1F4K75-_aj29Qyd1xaA19p-LLA0hbI9g0")
+names=("IEMOCAP" "MELD" "MOSEI" "MOSI" "TEXT")
+for ((i=0; i<${#array[@]}; i++)); do
+# for item in "${array[@]}"; do
+    item=${array[i]}
+    name=${names[i]}
     wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=${item}' -O- | sed -rn 's/.confirm=([0-9A-Za-z_]+)./\1\n/p')&id=${item}" && rm -rf /tmp/cookies.txt    
 done
 
 # Unzip
-# unzip -q ${filename}
+for ((i=0; i<${#names[@]}; i++)); do
+# for item in "${array[@]}"; do
+    name=${names[i]}
+    unzip -q ${name}
+done
 # rm ${filename}
 # cd
